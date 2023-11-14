22922a776284b97750b5f01e45509e6f705e52df
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-11-14 19:43:29 +0000

    update TaskEntity

--- src/main/java/gre/application/entities/task/TaskEntity.kt
+++ src/main/java/gre/application/entities/task/TaskEntity.kt
@@ -10,9 +10,9 @@ import java.time.LocalDate
 object TaskEntity : IntIdTable("task") {
 	val projectId: Column<EntityID<Int>> = reference("project_id", ProjectEntity)
 	val name: Column<String> = varchar("name", 100)
-	val description: Column<String> = largeText("description")
-	val status: Column<Status> = enumeration("status")
-	val priority: Column<Priority> = enumeration("priority")
+	val description: Column<String?> = largeText("description").nullable()
+	val status: Column<String> = varchar("status", 20)
+	val priority: Column<String> = varchar("priority", 20)
 	val successorId: Column<Int?> = integer("successor_id").nullable()
 	val createdDate: Column<LocalDate> = date("created_date").clientDefault { LocalDate.now() }
 	val deadline: Column<LocalDate> = date("deadline")
